63653452127acab15b0de26936b9a0d9b0b9cfd3
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-28 20:29:33 +0300

    feat: @EnableJpaAuiditing added

--- src/main/java/com/stzelas/gr/notes_app_api/NotesAppApiApplication.java
+++ src/main/java/com/stzelas/gr/notes_app_api/NotesAppApiApplication.java
@@ -1,8 +1,10 @@
-package com.stzelas.gr.notesapp;
+package com.stzelas.gr.notes_app_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NotesAppApiApplication {
 
