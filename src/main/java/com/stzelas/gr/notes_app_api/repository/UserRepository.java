5ad2536627acb0a71f1fe08f3b006b55b76d1dfd
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-28 20:30:51 +0300

    feat: user repository added

---
+++ src/main/java/com/stzelas/gr/notes_app_api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.stzelas.gr.notes_app_api.repository;
+
+import com.stzelas.gr.notes_app_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    Optional<User> findByUsername(String username);
+}
