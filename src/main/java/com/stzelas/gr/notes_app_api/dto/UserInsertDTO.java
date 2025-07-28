827fa42c7219e6ac7665b95ded3ba3360bfad5a8
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-28 20:30:35 +0300

    feat: user insert dto added as a record with bean validation

---
+++ src/main/java/com/stzelas/gr/notes_app_api/dto/UserInsertDTO.java
@@ -0,0 +1,20 @@
+package com.stzelas.gr.notes_app_api.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record UserInsertDTO(
+
+        @NotEmpty(message = "Το username δεν μπορεί να είναι κενό")
+        @Size(min = 4, max = 15, message = "Το username πρέπει να είναι μεταξύ 4 και 15 χαρακτήρων")
+        String username,
+        @NotEmpty(message = "Το password δεν μπορεί να είναι κενό")
+        @Size(min = 4, max = 15, message = "Το password πρέπει να είναι τουλάχιστον 6 χαρακτήρες")
+        String password,
+        @NotEmpty(message = "Το όνομα δεν μπορεί να είναι κενό")
+        @Size(min = 4, max = 15, message = "Το password όνομα να είναι τουλάχιστον 3 χαρακτήρες")
+        String firstname,
+        @NotEmpty(message = "Το επίθετο δεν μπορεί να είναι κενό")
+        @Size(min = 4, max = 15, message = "Το επίθετο πρέπει να είναι τουλάχιστον 3 χαρακτήρες")
+        String lastname
+) {}
