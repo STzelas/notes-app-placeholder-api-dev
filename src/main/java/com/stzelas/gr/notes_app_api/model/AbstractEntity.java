9962ff17fb97ccebfa0dc88c5af0df0f6f0d5a8c
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-28 20:28:20 +0300

    feat: abstract entity added

---
+++ src/main/java/com/stzelas/gr/notes_app_api/model/AbstractEntity.java
@@ -0,0 +1,32 @@
+package com.stzelas.gr.notes_app_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+}
