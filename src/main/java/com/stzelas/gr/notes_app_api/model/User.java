894207deec7b1fd827a27f8df27cb3dbd0fac509
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-28 20:30:22 +0300

    feat: @Entity and Lombok annotations added along with the Entity's fields

--- src/main/java/com/stzelas/gr/notes_app_api/model/User.java
+++ src/main/java/com/stzelas/gr/notes_app_api/model/User.java
@@ -1,12 +1,39 @@
-package com.stzelas.gr.notesapp.model;
+package com.stzelas.gr.notes_app_api.model;
 
+import com.stzelas.gr.notes_app_api.core.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-public class User implements UserDetails {
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User extends AbstractEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    private String firstname;
+    private String lastname;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+    private boolean isEnabled;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
