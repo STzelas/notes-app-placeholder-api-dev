750022338f459080e19d7c9758005eac41cf4f99
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-28 19:16:44 +0300

    Initial Commit

---
+++ src/main/java/com/stzelas/gr/notesapp/NotesAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.stzelas.gr.notesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stzelas/gr/notes_app_api/NotesAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stzelas.gr.notes_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
