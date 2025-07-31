d1c428599a8af0e7edfc1688702016b944b783a8
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-31 15:53:28 +0300

    feat: App Object Invalid Argument custom exception added

---
+++ src/main/java/com/stzelas/gr/notes_app_api/core/exceptions/AppObjectInvalidArgumentException.java
@@ -0,0 +1,10 @@
+package com.stzelas.gr.notes_app_api.core.exceptions;
+
+public class AppObjectInvalidArgumentException extends AppGenericException {
+    private static final String DEFAULT_CODE = "InvalidArgument";
+
+    public AppObjectInvalidArgumentException(String code, String message) {
+        super(code + DEFAULT_CODE, message);
+    }
+}
+
