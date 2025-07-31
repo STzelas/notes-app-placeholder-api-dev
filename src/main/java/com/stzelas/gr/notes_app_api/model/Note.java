f7bbd010290411a2bfe6af1510a474a40d2c0450
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-31 15:54:07 +0300

    feat: Note entity added with many-to-one relation with the user.

---
+++ src/main/java/com/stzelas/gr/notes_app_api/model/Note.java
@@ -0,0 +1,24 @@
+package com.stzelas.gr.notes_app_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "notes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Note extends AbstractEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String title;
+    String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
