c57405e97c3f2c128e084d311dd2d5b23ac9f108
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-31 15:53:49 +0300

    feat: mapper for Note entity and NotReadeOnlyDTO added

--- src/main/java/com/stzelas/gr/notes_app_api/mapper/Mapper.java
+++ src/main/java/com/stzelas/gr/notes_app_api/mapper/Mapper.java
@@ -1,7 +1,10 @@
 package com.stzelas.gr.notes_app_api.mapper;
 
+import com.stzelas.gr.notes_app_api.dto.NoteInsertDTO;
+import com.stzelas.gr.notes_app_api.dto.NoteReadOnlyDTO;
 import com.stzelas.gr.notes_app_api.dto.UserInsertDTO;
 import com.stzelas.gr.notes_app_api.dto.UserReadOnlyDTO;
+import com.stzelas.gr.notes_app_api.model.Note;
 import com.stzelas.gr.notes_app_api.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class Mapper {
+
 
     private final PasswordEncoder passwordEncoder;
 
@@ -28,6 +32,20 @@ public class Mapper {
         user.setFirstname(insertDTO.firstname());
         user.setLastname(insertDTO.lastname());
         return user;
+    }
+
+    public NoteReadOnlyDTO mapToNoteReadOnlyDTO(Note note) {
+        return new NoteReadOnlyDTO(
+                note.getTitle(),
+                note.getContent()
+        );
+    }
+
+    public Note mapToNoteEntity(NoteInsertDTO noteInsertDTO) {
+        Note note = new Note();
+        note.setTitle(noteInsertDTO.title());
+        note.setContent(noteInsertDTO.content());
+        return note;
     }
 
 }
