2a44440f1c61dc07d9912c696e11e14c97c85020
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-31 15:54:16 +0300

    feat: Note Insert DTO added with bean validation

---
+++ src/main/java/com/stzelas/gr/notes_app_api/dto/NoteInsertDTO.java
@@ -0,0 +1,15 @@
+package com.stzelas.gr.notes_app_api.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record NoteInsertDTO(
+        @NotNull(message = "Title must not be empty.")
+        @Size(min = 3, message = "Title must be at least 3 characters long.")
+        String title,
+
+        @NotNull(message = "Note content must not be empty.")
+        @Size(min = 3, message = "Content must be at least 3 characters long.")
+        String content
+) {}
