d679fac800ee75f6d489d0c0c09c5ef9ad3b818f
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-02 23:35:51 +0300

    feat: stereotype annotation added

--- src/main/java/com/stzelas/gr/notes_app_api/repository/UserRepository.java
+++ src/main/java/com/stzelas/gr/notes_app_api/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.stzelas.gr.notes_app_api.repository;
 import com.stzelas.gr.notes_app_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findByUsername(String username);
 }
