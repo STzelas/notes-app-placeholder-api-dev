5986424ab9bf07da100eb077e76536cf12e27f02
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-02 23:34:55 +0300

    feat: stereotype annotation added and import deleted

--- src/main/java/com/stzelas/gr/notes_app_api/repository/NoteRepository.java
+++ src/main/java/com/stzelas/gr/notes_app_api/repository/NoteRepository.java
@@ -2,8 +2,8 @@ package com.stzelas.gr.notes_app_api.repository;
 
 import com.stzelas.gr.notes_app_api.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 }
