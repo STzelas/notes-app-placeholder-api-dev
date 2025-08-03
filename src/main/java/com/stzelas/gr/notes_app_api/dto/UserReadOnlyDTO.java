b20ebcbefba544e65a2a98183461747fdb7a36f1
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-03 19:11:23 +0300

    chore: import delete

--- src/main/java/com/stzelas/gr/notes_app_api/dto/UserReadOnlyDTO.java
+++ src/main/java/com/stzelas/gr/notes_app_api/dto/UserReadOnlyDTO.java
@@ -1,7 +1,5 @@
 package com.stzelas.gr.notes_app_api.dto;
 
-import com.stzelas.gr.notes_app_api.core.enums.Role;
-
 public record UserReadOnlyDTO(
         String username,
         String firstname,
