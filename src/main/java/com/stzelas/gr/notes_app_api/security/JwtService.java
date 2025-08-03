54b5dc99743c175ae279d1ea71b1e1c01bbf114b
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-03 21:07:35 +0300

    feat: Jwt Service added with key generation

---
+++ src/main/java/com/stzelas/gr/notes_app_api/security/JwtService.java
@@ -0,0 +1,112 @@
+package com.stzelas.gr.notes_app_api.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    //    private String secretKey = System.getenv("SECRET_KEY");
+    //    private String secretKey = "FvArDZiJ1hvR9k3Ks1J6s8FqbmL6rRnlmTL5J3jNiT8";
+
+
+    private String secretKey = "";
+    private long jwtExpiration = 10800000;  // 3 hours in milliseconds
+
+//    if use refresh expiration token
+//    private long refreshExpiration = 604800000;
+
+    //    Strong security 384-bits = 48 bytes = 64 Base64URL characters
+
+    /**
+     * Generates a secret key with strong security 384-bits = 48 bytes = 64 Base64URL characters
+     */
+    public JwtService() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException();
+        }
+
+    }
+
+    public String generateToken(String username, String role) {
+        var claims = new HashMap<String, Object>();
+        claims.put("role", role);
+        return Jwts
+                .builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .and()
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String subject = extractSubject(token);
+        return (subject.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public String getStringClaim(String token, String claim) {
+        return extractAllClaims(token).get(claim, String.class);
+    }
+
+    public String extractSubject(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    /**
+     * Creates a HS256 Key. Key is an interface.
+     * Starting from secretKey we get a byte array
+     * of the secret. Then we get the {@link javax.crypto.SecretKey,
+     * class that implements the {@link Key } interface.
+     *
+     *
+     * @return  a SecretKey which implements Key.
+     */
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
