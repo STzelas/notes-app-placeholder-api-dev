621e17107695da421712322f2420f888bd3e6fb9
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-03 19:12:27 +0300

    feat(NoteRestController): user added to saveNote method, binding result removed added @Valid instead

--- src/main/java/com/stzelas/gr/notes_app_api/rest/NoteRestController.java
+++ src/main/java/com/stzelas/gr/notes_app_api/rest/NoteRestController.java
@@ -5,12 +5,17 @@ import com.stzelas.gr.notes_app_api.core.exceptions.ValidationException;
 import com.stzelas.gr.notes_app_api.dto.NoteInsertDTO;
 import com.stzelas.gr.notes_app_api.dto.NoteReadOnlyDTO;
 import com.stzelas.gr.notes_app_api.model.Note;
+import com.stzelas.gr.notes_app_api.model.User;
+import com.stzelas.gr.notes_app_api.model.UserPrincipal;
 import com.stzelas.gr.notes_app_api.service.NoteService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,13 +27,15 @@ public class NoteRestController {
     private final NoteService noteService;
 //    private final Logger LOGGER = LoggerFactory.getLogger(NoteRestController.class);
 
+
     @PostMapping("/notes/save")
-    public ResponseEntity<NoteReadOnlyDTO> saveNote(@RequestBody()NoteInsertDTO noteInsertDTO, BindingResult bindingResult) throws ValidationException, AppServerException {
-        if (bindingResult.hasErrors()) throw new ValidationException(bindingResult);
+    public ResponseEntity<NoteReadOnlyDTO> saveNote(@Valid @RequestBody()NoteInsertDTO noteInsertDTO,
+                                                    @AuthenticationPrincipal UserPrincipal principal)
+                            throws ValidationException, AppServerException {
 
         try {
-            NoteReadOnlyDTO noteReadOnlyDTO = noteService.saveNote(noteInsertDTO);
-            return new ResponseEntity<>(noteReadOnlyDTO, HttpStatus.OK);
+            NoteReadOnlyDTO savedNote = noteService.saveNote(noteInsertDTO, principal.getUser());
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedNote);
         } catch (Exception e) {
             throw new AppServerException("Note", "Note could not be created.");
         }
