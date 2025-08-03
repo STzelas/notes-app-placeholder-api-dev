36daca76ab2ffcee0e8e3c2d33f24bfe68c08a06
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-03 21:06:31 +0300

    feat: auth request dto added

---
+++ src/main/java/com/stzelas/gr/notes_app_api/dto/AuthenticationRequestDTO.java
@@ -0,0 +1,11 @@
+package com.stzelas.gr.notes_app_api.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public record AuthenticationRequestDTO (
+        @NotNull
+        String username,
+        @NotNull
+        String password
+        //  String token
+) {}
