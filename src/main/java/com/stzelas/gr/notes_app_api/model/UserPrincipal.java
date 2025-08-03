f15ac5df8389228c4d43718a84f50b15d346f462
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-03 19:13:07 +0300

    feat: lombok added to UserPrincipal

--- src/main/java/com/stzelas/gr/notes_app_api/model/UserPrincipal.java
+++ src/main/java/com/stzelas/gr/notes_app_api/model/UserPrincipal.java
@@ -1,5 +1,9 @@
 package com.stzelas.gr.notes_app_api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,13 +11,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserPrincipal implements UserDetails {
 
     private User user;
-
-    public UserPrincipal(User user) {
-        this.user = user;
-    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
