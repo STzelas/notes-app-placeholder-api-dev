b7aa967b64f40d9f4114f20ea9578fafc5d69278
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-12 17:56:24 +0300

    chore: imports removed

--- src/main/java/com/stzelas/gr/notes_app_api/dto/NoteInsertDTO.java
+++ src/main/java/com/stzelas/gr/notes_app_api/dto/NoteInsertDTO.java
@@ -1,7 +1,5 @@
 package com.stzelas.gr.notes_app_api.dto;
 
-import com.stzelas.gr.notes_app_api.model.User;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
