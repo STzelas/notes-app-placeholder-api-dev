4d224fb976e4dae02999083afd9a636020efe805
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-18 13:28:27 +0300

    feat: Todo insert dto added

---
+++ src/main/java/com/stzelas/gr/notes_app_api_dev/dto/TodoInsertDTO.java
@@ -0,0 +1,23 @@
+package com.stzelas.gr.notes_app_api_dev.dto;
+
+
+import com.stzelas.gr.notes_app_api_dev.core.enums.Importance;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record TodoInsertDTO(
+
+        @NotNull(message = "Description must not be empty.")
+        @Size(min = 3, message = "Description must be at least 3 characters long.")
+        String description,
+        Boolean isCompleted,
+
+        @NotNull(message = "Importance is required")
+        @Schema(
+                description = "Importance of the todo",
+                example = "MINOR",
+                allowableValues = {"MINOR", "MODERATE", "MAJOR"}
+        )
+        Importance importance
+) {}
