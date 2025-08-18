247c83e34900d1f5012f809c4b1598433db1ee7e
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-18 14:45:55 +0300

    feat: more bean validation added

--- src/main/java/com/stzelas/gr/notes_app_api_dev/dto/NoteInsertDTO.java
+++ src/main/java/com/stzelas/gr/notes_app_api_dev/dto/NoteInsertDTO.java
@@ -5,10 +5,12 @@ import jakarta.validation.constraints.Size;
 
 public record NoteInsertDTO(
         @NotNull(message = "Title must not be empty.")
+        @Size(min = 1, message = "Title must not be empty")
         @Size(max = 255, message = "Title must not be more than 255 characters")
         String title,
 
         @NotNull(message = "Note content must not be empty.")
+        @Size(min = 1, message = "Content must not be empty")
         @Size(max = 1000, message = "Content must not be more than 1000 characters")
         String content
 ) {}
