decf3d4454fa899821ae39ab41fa73d12cfdd766
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-19 23:12:42 +0300

    feat: TodoUpdateDTO record added

---
+++ src/main/java/com/stzelas/gr/notes_app_api_dev/dto/TodoUpdateDTO.java
@@ -0,0 +1,22 @@
+package com.stzelas.gr.notes_app_api_dev.dto;
+
+import com.stzelas.gr.notes_app_api_dev.core.enums.Importance;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record TodoUpdateDTO (
+        @NotNull(message = "Description must not be empty.")
+        @Size(min = 3, message = "Description must be at least 3 characters long.")
+        @Size(max = 255, message = "Description must not be more than 255 characters long.")
+        String description,
+
+        @NotNull(message = "Importance must not be empty")
+        @Schema(
+                description = "Importance of the todo",
+                example = "MINOR",
+                allowableValues = {"MINOR", "MODERATE", "MAJOR"}
+        )
+        Importance importance,
+        Boolean isCompleted
+){}
